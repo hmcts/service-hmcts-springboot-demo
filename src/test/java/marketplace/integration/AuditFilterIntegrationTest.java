4d9a517f402bdef45cabec0daae443a906dc9bf4
coling01 <colin.greenwood@hmcts.net>
2025-10-10 16:08:55 +0100

    Add AuditFilter to show that we can get the audit details from request and response

--- src/test/java/marketplace/integration/AuditFilterIntegrationTest.java
+++ src/test/java/marketplace/integration/AuditFilterIntegrationTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest()
 @AutoConfigureMockMvc
 @Slf4j
-class RootControllerIntegrationTest {
+class AuditFilterIntegrationTest {
 
     @Resource
     private MockMvc mockMvc;
