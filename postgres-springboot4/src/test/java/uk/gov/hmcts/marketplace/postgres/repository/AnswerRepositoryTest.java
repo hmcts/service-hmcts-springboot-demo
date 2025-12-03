2450847a4ad9859a71e52073fd26467b18172854
coling01 <colin.greenwood@hmcts.net>
2025-12-03 15:44:10 +0000

    demo: update spring boot4 postgres demo

--- postgres-springboot4/src/test/java/uk/gov/hmcts/marketplace/postgres/repository/AnswerRepositoryTest.java
+++ postgres-springboot4/src/test/java/uk/gov/hmcts/marketplace/postgres/repository/AnswerRepositoryTest.java
@@ -5,11 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import uk.gov.hmcts.marketplace.postgres.config.TestContainersInitialise;
 import uk.gov.hmcts.marketplace.postgres.domain.AnswerEntity;
 
