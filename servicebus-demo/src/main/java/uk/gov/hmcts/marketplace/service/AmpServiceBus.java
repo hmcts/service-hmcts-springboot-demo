d468caf65adce092281156dc17ea53d0b446a97c
coling01 <colin.greenwood@hmcts.net>
2026-01-20 06:55:41 +0000

    AzureServiceBus - split unit tests and integration tests

--- servicebus-demo/src/main/java/uk/gov/hmcts/marketplace/service/AmpServiceBus.java
+++ servicebus-demo/src/main/java/uk/gov/hmcts/marketplace/service/AmpServiceBus.java
@@ -1,9 +1,9 @@
 package uk.gov.hmcts.marketplace.service;
 
-import com.azure.messaging.servicebus.ServiceBusClientBuilder;
 import com.azure.messaging.servicebus.ServiceBusMessage;
 import com.azure.messaging.servicebus.ServiceBusReceiverClient;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -11,29 +11,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
+@AllArgsConstructor
 @Service
 public class AmpServiceBus {
-    private static final String CONNECTION_STRING = "Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
-    private static final String QUEUE_NAME = "queue.1";
-
-    ServiceBusClientBuilder clientBuilder = new ServiceBusClientBuilder()
-            .connectionString(CONNECTION_STRING);
+    private final ServiceBusSenderClient senderClient;
+    private final ServiceBusReceiverClient receiverClient;
 
     public void sendMessage(String message) {
-        ServiceBusSenderClient senderClient = clientBuilder
-                .sender()
-                .queueName(QUEUE_NAME)
-                .buildClient();
         senderClient.sendMessage(new ServiceBusMessage(message));
-        log.info("Sent message to queue {}", QUEUE_NAME);
+        log.info("Sent message to queue {}", senderClient.getIdentifier());
         senderClient.close();
     }
 
     public List<String> getMessages(int maxMessages) {
-        ServiceBusReceiverClient receiverClient = clientBuilder
-                .receiver()
-                .queueName(QUEUE_NAME)
-                .buildClient();
         List<String> messages = new ArrayList<>();
         receiverClient.receiveMessages(maxMessages).forEach(msg -> {
             messages.add(String.valueOf(msg.getBody()));
--- servicebus-demo/src/test/java/uk/gov/hmcts/marketplace/AmpServiceBusTest.java
+++ servicebus-demo/src/test/java/uk/gov/hmcts/marketplace/AmpServiceBusTest.java
@@ -1,27 +1,51 @@
 package uk.gov.hmcts.marketplace;
 
+import com.azure.core.util.IterableStream;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusReceiverClient;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.marketplace.service.AmpServiceBus;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AmpServiceBusTest {
 
+    @Mock
+    ServiceBusSenderClient senderClient;
+    @Mock
+    ServiceBusReceiverClient receiverClient;
+
     @InjectMocks
     AmpServiceBus ampServiceBus;
 
+    @Captor
+    ArgumentCaptor<ServiceBusMessage> messageCaptor;
+
     @Test
-    void sb_should_send_and_receive_messages() {
-        ampServiceBus.sendMessage("Message One");
-        ampServiceBus.sendMessage("Message Two");
+    void sb_should_send_messages() {
+        ampServiceBus.sendMessage("My message");
+        verify(senderClient).sendMessage(messageCaptor.capture());
+        assertThat(messageCaptor.getValue().getBody().toString()).isEqualTo("My message");
+        verify(senderClient).close();
+    }
+
+    @Test
+    void sb_should_receive_empty_messages() {
+        when(receiverClient.receiveMessages(2)).thenReturn(new IterableStream<>(new ArrayList<>()));
         List<String> messages = ampServiceBus.getMessages(2);
-        assertThat(messages.get(0)).isEqualTo("Message One");
-        assertThat(messages.get(1)).isEqualTo("Message Two");
+        assertThat(messages.size()).isEqualTo(0);
     }
 }
