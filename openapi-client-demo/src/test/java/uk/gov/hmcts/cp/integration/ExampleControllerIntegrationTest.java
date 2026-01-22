493ef318369304035cc62b02b229abb9466f63e6
coling01 <colin.greenwood@hmcts.net>
2026-01-22 08:34:34 +0000

    Rename openapi-client-demo

--- openapi-client-demo/src/test/java/uk/gov/hmcts/cp/integration/ExampleControllerIntegrationTest.java
+++ openapi-client-demo/src/test/java/uk/gov/hmcts/cp/integration/ExampleControllerIntegrationTest.java
@@ -4,14 +4,18 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import uk.gov.hmcts.cp.service.OpenApiConsumerService;
 import uk.gov.hmcts.cp.openapi.model.ExampleResponse;
+import uk.gov.hmcts.cp.service.OpenApiConsumerService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
@@ -28,13 +32,10 @@ class ExampleControllerIntegrationTest {
     @Autowired
     OpenApiConsumerService openApiConsumerService;
 
+    @SneakyThrows
     @Test
     void client_should_get_example() {
-        ResponseDefinitionBuilder mockResponse = aResponse()
-                .withStatus(HTTP_OK)
-                .withHeader("Content-Type", "application/json")
-                .withBody("{\"exampleId\":\"21\",\"exampleText\":\"Hello\"}".getBytes());
-        stubFor(WireMock.get(urlEqualTo("/example/" + 21)).willReturn(mockResponse));
+        stubGetResponse("/example/" + 21, "example-response.json");
 
         ExampleResponse response = openApiConsumerService.getExample(21L);
         assertThat(response.getExampleId()).isEqualTo(21L);
@@ -53,5 +54,19 @@ class ExampleControllerIntegrationTest {
         if (wireMockServer != null) {
             wireMockServer.stop();
         }
+    }
+
+    private void stubGetResponse(String url, String filename) {
+        ResponseDefinitionBuilder mockResponse = aResponse()
+                .withStatus(HTTP_OK)
+                .withHeader("Content-Type", "application/json")
+                .withBody(readJsonResponseFile(filename));
+        stubFor(WireMock.get(urlEqualTo(url)).willReturn(mockResponse));
+    }
+
+    @SneakyThrows
+    private String readJsonResponseFile(String filename) {
+        String folder = "src/test/resources/responses/";
+        return Files.readString(Path.of(folder + filename));
     }
 }
