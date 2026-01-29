ee128cca4119e35ed4fc41a6beb2dcadf907921b
coling01 <colin.greenwood@hmcts.net>
2026-01-29 09:12:25 +0000

    service-bus-topic: start to bring in dlq processing

--- servicebus-topic-demo/src/main/java/uk/gov/hmcts/marketplace/service/TopicService.java
+++ servicebus-topic-demo/src/main/java/uk/gov/hmcts/marketplace/service/TopicService.java
@@ -6,6 +6,8 @@ import com.azure.messaging.servicebus.ServiceBusProcessorClient;
 import com.azure.messaging.servicebus.ServiceBusReceivedMessage;
 import com.azure.messaging.servicebus.ServiceBusReceivedMessageContext;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
+import com.azure.messaging.servicebus.models.ServiceBusReceiveMode;
+import com.azure.messaging.servicebus.models.SubQueue;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -22,41 +24,91 @@ public class TopicService {
     private ServiceBusConfigService configService;
     private ClientService clientService;
 
-    public void sendMessage(String message) {
+    public void sendMessage(String topicName, String message) {
         ServiceBusSenderClient senderClient = configService
                 .clientBuilder()
                 .sender()
-                .topicName("topic.1")
+                .topicName(topicName)
                 .buildClient();
         senderClient.sendMessage(new ServiceBusMessage(message));
     }
 
     @SneakyThrows
-    public void processMessages(int processingSecs) {
+    public void processMessages(String topicName, String subscriptionName, int processingSecs) {
         ServiceBusProcessorClient processorClient = configService
                 .clientBuilder()
                 .processor()
-                .topicName("topic.1")
-                .subscriptionName("subscription.1")
-                .processMessage(context -> processMessage(context))
-                .processError(context -> processError(context))
+                .topicName(topicName)
+                .subscriptionName(subscriptionName)
+                .processMessage(context -> processMessage(topicName, subscriptionName, context))
+                .processError(context -> processError(topicName, subscriptionName, context))
                 .buildProcessorClient();
 
-        System.out.println("Starting the processor");
+        log.info("Starting the processor for {}", subscriptionName);
         processorClient.start();
 
         TimeUnit.SECONDS.sleep(processingSecs);
-        System.out.println("Stopping and closing the processor");
+        log.info("Stopping and closing the processor for {}", subscriptionName);
         processorClient.close();
     }
 
-    private void processMessage(ServiceBusReceivedMessageContext context) {
-        ServiceBusReceivedMessage message = context.getMessage();
-        log.info("Processing messageId:{} {}", message.getMessageId(), message.getBody());
-        clientService.receiveMessage(String.valueOf(message.getBody()));
+    @SneakyThrows
+    public int countDeadLetterQueue(String topicName, String subscriptionName, int processingSecs) {
+        int count = 0;
+        ServiceBusProcessorClient processorClient = configService
+                .clientBuilder()
+                .processor()
+                .topicName(topicName)
+                .subscriptionName(subscriptionName)
+                .subQueue(SubQueue.DEAD_LETTER_QUEUE)
+                .receiveMode(ServiceBusReceiveMode.PEEK_LOCK)
+                .processMessage(context -> peekMessage(topicName, subscriptionName, context, count))
+                .processError(context -> processError(topicName, subscriptionName, context))
+                .buildProcessorClient();
+
+        log.info("Starting the processor for {}", subscriptionName);
+        processorClient.start();
+
+        TimeUnit.SECONDS.sleep(processingSecs);
+        log.info("Stopping and closing the processor for {} with {} processed", subscriptionName, count);
+        processorClient.close();
+        return count;
     }
 
-    private static void processError(ServiceBusErrorContext context) {
+    @SneakyThrows
+    public void processDeadLetterQueue(String topicName, String subscriptionName, int processingSecs) {
+        ServiceBusProcessorClient processorClient = configService
+                .clientBuilder()
+                .processor()
+                .topicName(topicName)
+                .subscriptionName(subscriptionName)
+                .subQueue(SubQueue.DEAD_LETTER_QUEUE)
+                .receiveMode(ServiceBusReceiveMode.PEEK_LOCK)
+                .processMessage(context -> processMessage(topicName, subscriptionName, context))
+                .processError(context -> processError(topicName, subscriptionName, context))
+                .buildProcessorClient();
+
+        log.info("Starting the processor for {}", subscriptionName);
+        processorClient.start();
+
+        TimeUnit.SECONDS.sleep(processingSecs);
+        log.info("Stopping and closing the processor for {}", subscriptionName);
+        processorClient.close();
+    }
+
+    private void peekMessage(String topicName, String subscriptionName, ServiceBusReceivedMessageContext context, int count) {
+        ServiceBusReceivedMessage message = context.getMessage();
+        log.info("Peeking {}/{} messageId:{} {}", topicName, subscriptionName, message.getMessageId(), message.getBody());
+        count++;
+    }
+
+    private void processMessage(String topicName, String subscriptionName, ServiceBusReceivedMessageContext context) {
+        ServiceBusReceivedMessage message = context.getMessage();
+        log.info("Processing {}/{} messageId:{} {}", topicName, subscriptionName, message.getMessageId(), message.getBody());
+        clientService.receiveMessage(topicName, subscriptionName, String.valueOf(message.getBody()));
+    }
+
+    private static void processError(String topicName, String subscriptionName, ServiceBusErrorContext context) {
         // We need to properly handle the error ... leave it on the queue / send to DLQ
         log.error("error processing subscription message.", context.getException());
     }
