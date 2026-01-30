cac89bed9a397d35a21b9c07229f4a6f6feb3e48
coling01 <colin.greenwood@hmcts.net>
2026-01-30 10:09:21 +0000

    service-bus-topic tweaks
    Use separate gradle task of dockerTest as well as unitTest and integrationTest

--- gradle-test-demo/src/test/java/DemoDockerTest.java
+++ gradle-test-demo/src/test/java/DemoDockerTest.java
@@ -4,11 +4,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
-public class DemoIntegrationTest {
+public class DemoDockerTest {
 
     @Test
-    void int_test_should_start_springboot_and_run_ok(){
-        log.info("Integration test running .. without docker");
+    void int_test_should_start_docker_and_springboot_and_run_ok(){
+        log.info("Docker test running .. without docker");
         throw new RuntimeException("Bad integration test");
     }
 }
