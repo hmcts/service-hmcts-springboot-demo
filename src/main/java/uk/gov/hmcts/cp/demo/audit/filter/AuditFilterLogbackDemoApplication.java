561d3ee203d676b5645fae4876ebf437cb514fc0
samirgarg <samir.garg@hmcts.net>
2026-03-13 09:44:12 +0000

    Initial spring boot project with root controller and basic logging

---
+++ src/main/java/uk/gov/hmcts/cp/demo/audit/filter/AuditFilterLogbackDemoApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.cp.demo.audit.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditFilterLogbackDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditFilterLogbackDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/cp/demo/audit/filter/AuditFilterLogbackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.cp.demo.audit.filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditFilterLogbackDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
