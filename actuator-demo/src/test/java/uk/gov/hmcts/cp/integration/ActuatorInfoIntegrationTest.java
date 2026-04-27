c0bf181402360584612419c63b87bf2b39e298ce
Colin Greenwood <colingreenwood@scrumconnect.com>
2026-04-27 12:37:31 +0100

    add gorylenko to actuator info demo

--- actuator-demo/src/test/java/uk/gov/hmcts/cp/integration/ActuatorInfoIntegrationTest.java
+++ actuator-demo/src/test/java/uk/gov/hmcts/cp/integration/ActuatorInfoIntegrationTest.java
@@ -15,23 +15,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@TestPropertySource(properties = {"ARTEFACT_VERSION=v1.0.1"})
 @Slf4j
 class ActuatorInfoIntegrationTest {
 
     @Resource
     private MockMvc mockMvc;
 
-    static {
-        System.setProperty("ARTEFACT_VERSION", "v1.0.1");
-    }
-
     @Test
-    void actuator_info_should_display_version_from_environment() throws Exception {
+    void actuator_info_should_include_gorylenko_git_info() throws Exception {
         mockMvc.perform(get("/actuator/info"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.build.name").value("actuator-demo"))
-                .andExpect(jsonPath("$.build.version").value("v1.0.1"));
+                .andExpect(jsonPath("$.build.version").value("1.0.0"))
+                .andExpect(jsonPath("$.build.time").exists())
+                .andExpect(jsonPath("$.git.branch").exists())
+                .andExpect(jsonPath("$.git.commit.id").exists())
+                .andExpect(jsonPath("$.git.commit.time").exists());
     }
 }
